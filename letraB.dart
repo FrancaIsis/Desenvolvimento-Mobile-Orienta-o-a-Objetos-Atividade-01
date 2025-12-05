60791d51691829bba9e7461e5a6f5a681dcf4ead
FrancaIsis <isisdej@outlokk.com>
2025-12-04 21:43:46 -0300

    letraB

---
+++ letraB.dart
@@ -0,0 +1,57 @@
+/* Faça um programa que peça um ano qualquer. O programa deverá calcular
+ e imprimir a idade.
+ */
+import 'dart:io';
+
+class Idade {
+  int anoNasc;
+  int anoAtual;
+
+  // Método construtor com parametro obrigatorio
+  Idade({required this.anoNasc, required this.anoAtual});
+
+  // Método para calcular a idade
+  int calcularIdade() {
+    return (anoAtual - anoNasc);
+  }
+
+  // Função para ler o ano de nascimento do usuario
+  int lerAno(String msg) {
+    String? input;
+    while (true) {
+      stdout.write(msg);
+      input = stdin.readLineSync();
+
+      if (input == null || input.trim().isEmpty) {
+        print("Informe o ano de nascimento.");
+        continue;
+      }
+      //verificando os 4 digitos
+      RegExp exp = RegExp(r'^\d{4}$');
+      if (!exp.hasMatch(input)) {
+        print("Informe o ano com 4 dígitos. Ex.: 1985.");
+        continue;
+      }
+
+      // converte para inteiro
+      int ano = int.parse(input);
+
+      // pega o ano atual
+      int anoAtual = DateTime.now().year;
+
+      if (ano > anoAtual) {
+        print("O ano de nascimento não pode ser maior que o ano corrente.");
+        continue;
+      }
+      return ano;
+    }
+  }
+}
+
+// programa principal
+void main() {
+  Idade suaIdade = Idade(anoNasc: 0, anoAtual: 0);
+  print("Calculando a idade");
+  suaIdade.anoAtual = suaIdade.lerAno("Ano corrente: ");
+  suaIdade.anoNasc = suaIdade.lerAno("Digite seu ano de nascimento (YYYY): ");
+}
